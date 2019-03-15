001d84892a00ec661cf9b6577e671b36d6f26cc0
acbca <idacbca@126.com>
2019-03-15 13:25:46 +0800

    test

--- admin/index/controller/Goods.php
+++ admin/index/controller/Goods.php
@@ -10,6 +10,11 @@ class Goods extends Controller
 	}
 
 	public function product_category_add(){
+		$data = db('goods_type')->field("*, concat(path, ',', id) as paths")->order('paths')->select();
+		foreach ($data as $k => $v) {
+			$data[$k]['name'] = str_repeat("|------", $v['level']).$v['name'];
+		}
+		$this->assign('data', $data);
 		return $this->fetch();
 	}
 }
