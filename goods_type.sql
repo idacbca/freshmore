5a49e37d12bd92acc5e1f5bb51f1f2241b788c45
acbca <idacbca@126.com>
2019-03-15 16:27:48 +0800

    增加数据表

---
+++ goods_type.sql
@@ -0,0 +1,8 @@
+INSERT INTO `goods_type` VALUES (1, '服装', 1, '0,1', 1);
+INSERT INTO `goods_type` VALUES (2, '西服', 1, '0,1,2', 2);
+INSERT INTO `goods_type` VALUES (3, '鞋', 0, '0,3', 1);
+INSERT INTO `goods_type` VALUES (27, '女装', 1, '0,1,27', 2);
+INSERT INTO `goods_type` VALUES (30, '小裙子', 27, '0,1,27,30', 3);
+INSERT INTO `goods_type` VALUES (31, '防弹衣', 0, '0,31', 1);
+INSERT INTO `goods_type` VALUES (32, '凯夫拉', 31, '0,31,32', 2);
+INSERT INTO `goods_type` VALUES (33, '小皮鞋', 3, '0,3,33', 2);
