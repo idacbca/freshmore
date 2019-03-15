48583a898d597dcf92fb268ea722ce90e9378de1
acbca <idacbca@126.com>
2019-03-15 16:08:53 +0800

    完成添加分类功能

--- admin/index/controller/Goods.php
+++ admin/index/controller/Goods.php
@@ -17,4 +17,44 @@ class Goods extends Controller
 		$this->assign('data', $data);
 		return $this->fetch();
 	}
+
+
+	// 向数据库添加分类信息
+	public function goods_type_add(){
+		$data['name'] = $_POST['name'];
+		$data['pid'] = $_POST['pid'];
+		$m = db('goods_type');
+		if($data['name'] != "" && $data['pid'] != 0){
+			$path = $m->field("path")->find($data['pid']);
+			$data['path'] = $path['path'];
+			$data['level'] = substr_count($data['path'],',');
+			$re = $m->insert($data);
+			$id = db('user')->getLastInsID();//获取此次插入分类的id
+			$path['id'] = $id;
+			$path['path'] = $data['path'].','.$id;
+			$path['level'] = substr_count($path['path'],',');
+			$res = $m->update($path);
+			if($res){
+				echo '<script>alert("添加成功！");parent.location.href="product_category"</script>';
+			} else{
+				echo '<script>alert("添加失败！");parent.location.href="product_category"</script>';
+			}
+		}else if ($data['name'] != "" && $data['pid'] == 0){
+			$path = $m->field("path")->find($data['pid']);
+			$data['path'] = $data['pid'];
+			$data['level'] = 1;
+			$re = $m->insert($data);
+			$id = db('user')->getLastInsID();//获取此次插入分类的id
+			$path['id'] = $id;
+			$path['path'] = $data['path'].','.$id;
+			$res = $m->update($path);
+			if($res){
+				echo '<script>alert("添加成功！");parent.location.href="product_category"</script>';
+			} else{
+				echo '<script>alert("添加失败！");parent.location.href="product_category"</script>';
+			}
+		}else {
+			echo '<script>alert("分类名不能为空！");parent.location.href="product_category"</script>';
+		}
+	}
 }
