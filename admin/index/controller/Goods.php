92fccd5a87a8e1553983a1603c10aa87b987e641
acbca <idacbca@126.comm>
2019-03-16 21:17:33 +0800

    更新商品分类功能

--- admin/index/controller/Goods.php
+++ admin/index/controller/Goods.php
@@ -18,42 +18,64 @@ class Goods extends Controller
 		return $this->fetch();
 	}
 
+	// 获取分类信息
+	public function product_category_ajax(){
+		$db = db('goods_type');
+		$data = $db->field('id, pid, name')->select();
+		echo json_encode($data);
+	}
 
-	// 向数据库添加分类信息
+	// 删除分类信息
+	public function product_category_del(){
+		$id = $_GET['id'];
+		$db = db('goods_type');
+		$data = $db->where('pid', $id)->find();
+		if($data){
+			$str = '分类下存在子分类，不能删除！';
+			echo json_encode($str);
+		} else{
+			$re = $db->where('id', $id)->delete();
+			if($re){
+				echo 1;
+			}
+		}
+	}
+
+	// 添加分类信息
 	public function goods_type_add(){
 		$data['name'] = $_POST['name'];
 		$data['pid'] = $_POST['pid'];
-		$m = db('goods_type');
+		$db = db('goods_type');
 		if($data['name'] != "" && $data['pid'] != 0){
-			$path = $m->field("path")->find($data['pid']);
+			$path = $db->field("path")->find($data['pid']);
 			$data['path'] = $path['path'];
 			$data['level'] = substr_count($data['path'],',');
-			$re = $m->insert($data);
+			$re = $db->insert($data);
 			$id = db('user')->getLastInsID();//获取此次插入分类的id
 			$path['id'] = $id;
 			$path['path'] = $data['path'].','.$id;
 			$path['level'] = substr_count($path['path'],',');
-			$res = $m->update($path);
+			$res = $db->update($path);
 			if($res){
 				echo '<script>alert("添加成功！");parent.location.href="product_category"</script>';
 			} else{
 				echo '<script>alert("添加失败！");parent.location.href="product_category"</script>';
 			}
 		}else if ($data['name'] != "" && $data['pid'] == 0){
-			$path = $m->field("path")->find($data['pid']);
+			$path = $db->field("path")->find($data['pid']);
 			$data['path'] = $data['pid'];
 			$data['level'] = 1;
-			$re = $m->insert($data);
+			$re = $db->insert($data);
 			$id = db('user')->getLastInsID();//获取此次插入分类的id
 			$path['id'] = $id;
 			$path['path'] = $data['path'].','.$id;
-			$res = $m->update($path);
+			$res = $db->update($path);
 			if($res){
 				echo '<script>alert("添加成功！");parent.location.href="product_category"</script>';
 			} else{
 				echo '<script>alert("添加失败！");parent.location.href="product_category"</script>';
 			}
-		}else {
+		} else{
 			echo '<script>alert("分类名不能为空！");parent.location.href="product_category"</script>';
 		}
 	}
--- xianduoduo.sql
+++ xianduoduo.sql
@@ -1,45 +1,46 @@
-/*
- Navicat Premium Data Transfer
+-- --------------------------------------------------------
+-- 主机:                           127.0.0.1
+-- 服务器版本:                        5.7.25 - MySQL Community Server (GPL)
+-- 服务器操作系统:                      Win64
+-- HeidiSQL 版本:                  9.5.0.5284
+-- --------------------------------------------------------
 
- Source Server         : test
- Source Server Type    : MySQL
- Source Server Version : 50725
- Source Host           : localhost:3306
- Source Schema         : xianduoduo
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
- Target Server Type    : MySQL
- Target Server Version : 50725
- File Encoding         : 65001
 
- Date: 15/03/2019 16:49:42
-*/
+-- 导出 xianduoduo 的数据库结构
+CREATE DATABASE IF NOT EXISTS `xianduoduo` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `xianduoduo`;
 
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
-
--- ----------------------------
--- Table structure for goods_type
--- ----------------------------
-DROP TABLE IF EXISTS `goods_type`;
-CREATE TABLE `goods_type`  (
-  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+-- 导出  表 xianduoduo.goods_type 结构
+CREATE TABLE IF NOT EXISTS `goods_type` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
   `pid` int(11) NOT NULL,
-  `path` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `path` varchar(255) NOT NULL,
   `level` int(255) NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 34 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE=InnoDB AUTO_INCREMENT=42 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
--- ----------------------------
--- Records of goods_type
--- ----------------------------
-INSERT INTO `goods_type` VALUES (1, '服装', 1, '0,1', 1);
-INSERT INTO `goods_type` VALUES (2, '西服', 1, '0,1,2', 2);
-INSERT INTO `goods_type` VALUES (3, '鞋', 0, '0,3', 1);
-INSERT INTO `goods_type` VALUES (27, '女装', 1, '0,1,27', 2);
-INSERT INTO `goods_type` VALUES (30, '小裙子', 27, '0,1,27,30', 3);
-INSERT INTO `goods_type` VALUES (31, '防弹衣', 0, '0,31', 1);
-INSERT INTO `goods_type` VALUES (32, '凯夫拉', 31, '0,31,32', 2);
-INSERT INTO `goods_type` VALUES (33, '小皮鞋', 3, '0,3,33', 2);
+-- 正在导出表  xianduoduo.goods_type 的数据：~9 rows (大约)
+DELETE FROM `goods_type`;
+/*!40000 ALTER TABLE `goods_type` DISABLE KEYS */;
+INSERT INTO `goods_type` (`id`, `name`, `pid`, `path`, `level`) VALUES
+	(1, '服装', 1, '0,1', 1),
+	(2, '西服', 1, '0,1,2', 2),
+	(3, '鞋', 0, '0,3', 1),
+	(27, '女装', 1, '0,1,27', 2),
+	(30, '小裙子', 27, '0,1,27,30', 3),
+	(31, '防弹衣', 0, '0,31', 1),
+	(34, '运动鞋', 3, '0,3,34', 2),
+	(35, '拼多多运动鞋', 34, '0,3,34,35', 3),
+	(41, '凯夫拉', 31, '0,31,41', 2);
+/*!40000 ALTER TABLE `goods_type` ENABLE KEYS */;
 
-SET FOREIGN_KEY_CHECKS = 1;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
