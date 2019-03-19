b261122a456755f45061c7ebf97f4cfc91108e60
idacbca <idacbca@126.com>
2019-03-19 13:58:38 +0800

    更新

--- xianduoduo.sql
+++ xianduoduo.sql
@@ -1,46 +1,77 @@
--- --------------------------------------------------------
--- 主机:                           127.0.0.1
--- 服务器版本:                        5.7.25 - MySQL Community Server (GPL)
--- 服务器操作系统:                      Win64
--- HeidiSQL 版本:                  9.5.0.5284
--- --------------------------------------------------------
+/*
+ Navicat Premium Data Transfer
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+ Source Server         : test
+ Source Server Type    : MySQL
+ Source Server Version : 50725
+ Source Host           : localhost:3306
+ Source Schema         : xianduoduo
 
+ Target Server Type    : MySQL
+ Target Server Version : 50725
+ File Encoding         : 65001
 
--- 导出 xianduoduo 的数据库结构
-CREATE DATABASE IF NOT EXISTS `xianduoduo` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `xianduoduo`;
+ Date: 19/03/2019 13:58:12
+*/
 
--- 导出  表 xianduoduo.goods_type 结构
-CREATE TABLE IF NOT EXISTS `goods_type` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for goods
+-- ----------------------------
+DROP TABLE IF EXISTS `goods`;
+CREATE TABLE `goods`  (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '商品自增id',
+  `goodsname` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '商品的名字',
+  `tid` int(11) NULL DEFAULT NULL COMMENT '分类id',
+  `tpid` int(255) NULL DEFAULT NULL COMMENT '分类路径',
+  `unit` char(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '商品的单位',
+  `attributes` enum('7','6','5','4','3','2','1') CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '商品的属性,1、推荐 2、新上 3、热卖 4、促销 5、包邮 6、限时卖 7、不参与会员折扣',
+  `imagepath` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '商品图片id',
+  `number` int(11) NULL DEFAULT NULL COMMENT '商品编号',
+  `barcode` int(255) NULL DEFAULT NULL COMMENT '条形码',
+  `curprice` int(11) NULL DEFAULT NULL COMMENT '现价',
+  `oriprice` int(11) NULL DEFAULT NULL COMMENT '市场价',
+  `cosprice` int(11) NULL DEFAULT NULL COMMENT '成本价',
+  `inventory` int(255) NULL DEFAULT NULL COMMENT '库存量',
+  `restrict` int(255) NULL DEFAULT NULL COMMENT '限制购买量',
+  `already` int(255) NULL DEFAULT NULL COMMENT '已经购买量',
+  `freight` int(255) NULL DEFAULT NULL COMMENT '运费',
+  `status` enum('1','0') CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '是否上架,0为上架,1为下架,默认0上架',
+  `reorder` int(255) NULL DEFAULT NULL,
+  `text` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of goods
+-- ----------------------------
+INSERT INTO `goods` VALUES (1, 'yy', 27, 1, '11', '1', '', 11, 11, 1, 1, 11, 1, 1, 1, 1, '0', 1, '<p>1212</p>');
+
+-- ----------------------------
+-- Table structure for goods_type
+-- ----------------------------
+DROP TABLE IF EXISTS `goods_type`;
+CREATE TABLE `goods_type`  (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `pid` int(11) NOT NULL,
-  `path` varchar(255) NOT NULL,
+  `path` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `level` int(255) NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=42 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 35 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
--- 正在导出表  xianduoduo.goods_type 的数据：~9 rows (大约)
-DELETE FROM `goods_type`;
-/*!40000 ALTER TABLE `goods_type` DISABLE KEYS */;
-INSERT INTO `goods_type` (`id`, `name`, `pid`, `path`, `level`) VALUES
-	(1, '服装', 1, '0,1', 1),
-	(2, '西服', 1, '0,1,2', 2),
-	(3, '鞋', 0, '0,3', 1),
-	(27, '女装', 1, '0,1,27', 2),
-	(30, '小裙子', 27, '0,1,27,30', 3),
-	(31, '防弹衣', 0, '0,31', 1),
-	(34, '运动鞋', 3, '0,3,34', 2),
-	(35, '拼多多运动鞋', 34, '0,3,34,35', 3),
-	(41, '凯夫拉', 31, '0,31,41', 2);
-/*!40000 ALTER TABLE `goods_type` ENABLE KEYS */;
+-- ----------------------------
+-- Records of goods_type
+-- ----------------------------
+INSERT INTO `goods_type` VALUES (1, '服装', 1, '0,1', 1);
+INSERT INTO `goods_type` VALUES (2, '西服', 1, '0,1,2', 2);
+INSERT INTO `goods_type` VALUES (3, '鞋', 0, '0,3', 1);
+INSERT INTO `goods_type` VALUES (27, '女装', 1, '0,1,27', 2);
+INSERT INTO `goods_type` VALUES (31, '防弹衣', 0, '0,31', 1);
+INSERT INTO `goods_type` VALUES (32, '凯夫拉', 31, '0,31,32', 2);
+INSERT INTO `goods_type` VALUES (33, '小皮鞋', 3, '0,3,33', 2);
+INSERT INTO `goods_type` VALUES (34, '小裙子', 27, '0,1,27,34', 3);
 
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+SET FOREIGN_KEY_CHECKS = 1;
