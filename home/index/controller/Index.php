ff0eecdb4995ef1746318e6aed0a6d1b01583cc5
Winniewy <914386610@qq.com>
2019-04-09 15:58:17 +0800

    前台商品分类循环

--- home/index/controller/Index.php
+++ home/index/controller/Index.php
@@ -7,7 +7,35 @@ class Index extends Controller
 {
     public function index()
     {
+        $m=db('goods_type');
+        $type=$m->where("pid=0")->select();//获取一级分类
+        $type2=array();
+        $type3=array();
+        foreach($type as $key=>$value){
+             $type[$key]['chlid']=array();
+             $type2=$m->where("pid=".$value['id'])->select();//获取二级分类
+
+             foreach($type2 as $k=>$v){
+                $type[$key]['chlid'][$k]['chlid2']=array(); //组装三级分类的数组
+
+                 array_push($type[$key]['chlid'],$v);//合并一级二级分类
+
+                $type3=$m->where("pid=".$v['id'])->select(); //获取三级分类
+                
+                foreach($type3 as $v2){
+
+                    array_push($type[$key]['chlid'][$k]['chlid2'],$v2);//合并一二三级分类
+                }
+
+                
+             }
+
+        }
+        
+       // var_dump($type);
+
         return $this->fetch();
+        
     }
 
     public function about_us()
